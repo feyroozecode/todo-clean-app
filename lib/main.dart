69ea205b8e8c9807979dd7d54fe20b639aeca94a
feyroozecode <ibrahmad18@gmail.com>
2025-02-18 17:05:33 +0100

    first cmmit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/homepage.dart';
+
+// lancement de l'app -> Dart lance l'application Flutter
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TodoList',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'TodoList'),
+    );
+  }
+}
